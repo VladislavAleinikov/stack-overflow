93cdb5ddc072c15c3c4afe5bb9c8d54c50dd4f54
Vlad Aleynikov <vladislav.aleinikov@innowise.com>
2025-08-13 12:49:54 +0300

    init project

---
+++ src/app/entrypoint/main.tsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { StyledEngineProvider } from "@mui/material/styles";
+import GlobalStyles from "@mui/material/GlobalStyles";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import '../styles/global.css'
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <StyledEngineProvider enableCssLayer>
+      <GlobalStyles styles="@layer theme, base, mui, components, utilities;" />
+
+    </StyledEngineProvider>
+  </StrictMode>
+);
