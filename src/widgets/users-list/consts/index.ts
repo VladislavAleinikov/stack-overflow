18cb1e3d09fee4b9de3e1c255002dd0e7c8c3bc9
Vlad Aleynikov <vladislav.aleinikov@innowise.com>
2025-08-25 17:16:01 +0300

    fix users sort options

--- src/widgets/users-list/consts/index.ts
+++ src/widgets/users-list/consts/index.ts
@@ -1,5 +1,8 @@
 export const sortOptions: [string, string][] = [
-  ["Default", "username:ASC"],
+  ["Username A-Z", "username:ASC"],
+  ["Username Z-A", "username:DESC"],
   ["New first", "id:DESC"],
   ["Old first", "id:ASC"],
+  ["Admins first", "role:ASC"],
+  ["Admins last", "role:DESC"]
 ];
