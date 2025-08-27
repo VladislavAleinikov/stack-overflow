efa71ae27f437bf10627095121ff71d1857225fc
Vlad Aleynikov <vladislav.aleinikov@innowise.com>
2025-08-27 15:05:38 +0300

    add user field to comment and answer types

--- src/shared/types/answer.ts
+++ src/shared/types/answer.ts
@@ -1,5 +1,8 @@
+import type { User } from ".";
+
 export type Answer = {
   id: number;
   content: string;
   isCorrect: boolean;
+  user: User
 };
--- src/shared/types/comment.ts
+++ src/shared/types/comment.ts
@@ -1,4 +1,7 @@
+import type { User } from ".";
+
 export type Comment = {
   id: number;
   content: string;
+  user: User;
 };
